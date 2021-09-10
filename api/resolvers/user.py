cec84304c04316b41e2cad0451c9d4d6bff6abac
Daniel Hidalgo <dhidalgosanchez96@gmail.com>
2021-09-10 17:43:03 +0200

    feat: add first user gQL schema

--- api/resolvers/user.py
+++ api/resolvers/user.py
@@ -1,6 +1,53 @@
-def resolve_user(obj, info):
+""" Query resolvers """
+
+def resolve_user(obj, info, id=None):
     return {
         "email": "email 1",
         "username": "username 1",
     }
-    
+
+""" Mutation resolvers """
+
+def resolve_login(*_, input):
+    return {
+        "error": "",
+        "status": "OK",
+        "token": "",
+        "user": {
+            "email": "",
+            "username": "",
+        }
+    }
+
+def resolve_logout(obj, info):
+    return {
+        "error": "",
+        "status": ""
+    }
+
+def resolve_signup(*_, input):
+    return {
+        "error": "",
+        "status": "OK",
+        "token": "",
+        "user": {
+            "email": "",
+            "username": "",
+        }
+    }
+
+def resolve_update_user(*_, input):
+    return {
+        "error": "",
+        "status": "OK",
+        "user": {
+            "email": "",
+            "username": "",
+        }
+    }
+
+def resolve_update_password(*_, input):
+    return {
+        "error": "",
+        "status": ""
+    }
--- api/schema.py
+++ api/schema.py
@@ -1,18 +1,30 @@
-from ariadne import load_schema_from_path, make_executable_schema, QueryType
+from ariadne import load_schema_from_path, make_executable_schema, QueryType, MutationType
 
-from .resolvers.user import resolve_user
+from .resolvers.user import (
+  resolve_user, resolve_login, resolve_logout, resolve_signup, 
+  resolve_update_password, resolve_update_user
+)
 from .resolvers.game import resolve_explore_games, resolve_search_games, resolve_user_games
 
 type_defs = load_schema_from_path('./api/types/')
 
 query = QueryType()
+mutation = MutationType()
 
-# User
+""" User """
+# Queries
 query.set_field('user', resolve_user)
+# Mutations
+mutation.set_field('login', resolve_login)
+mutation.set_field('logout', resolve_logout)
+mutation.set_field('signUp', resolve_signup)
+mutation.set_field('updateUser', resolve_update_user)
+mutation.set_field('updatePassword', resolve_update_password)
 
-#Game
+""" Game """
+# Queries
 query.set_field('exploreGames', resolve_explore_games)
 query.set_field('searchGames', resolve_search_games)
 query.set_field('userGames', resolve_user_games)
 
-schema = make_executable_schema(type_defs, [query])
+schema = make_executable_schema(type_defs, [query, mutation])
